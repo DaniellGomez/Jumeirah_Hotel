fbfbb7a1345e49a4e1ecb291eafcd8e7a1f2f924
Camilo Urrego <ceurrego@bancofalabella.com.co>
2022-08-10 23:04:55 -0500

    prepared statements habitaciones, reservaciones y contacto

--- database.py
+++ database.py
@@ -8,8 +8,11 @@ def sql_connection():
     except Error:
         print(Error)
 
+
+#habitaciones
+
 def sql_insert_habitacion(num_hab,capacidad,acomodacion,bano):
-    strsql="INSERT INTO habitaciones (num_hab,hab_capacidad,hab_acomodacion,hab_bano) VALUES('"+num_hab+"' , '"+capacidad+"', '"+acomodacion+"', '"+bano+"');"
+    strsql=("INSERT INTO habitaciones (num_habitacion,hab_capacidad,hab_acomodacion,hab_bano) VALUES(?,?,?,?)",(num_hab,capacidad,acomodacion,bano))
     print(strsql)
     con=sql_connection()
     cursor_Obj=con.cursor()
@@ -18,7 +21,7 @@ def sql_insert_habitacion(num_hab,capacidad,acomodacion,bano):
     con.close()
 
 def sql_select_habitacion(num_hab):
-    strsql="SELECT * FROM habitaciones WHERE num_habitacion = '"+num_hab+"';"
+    strsql=("SELECT * FROM habitaciones WHERE num_habitacion = ?",(num_hab))
     print(strsql)
     con=sql_connection()
     cursor_Obj=con.cursor()
@@ -28,7 +31,7 @@ def sql_select_habitacion(num_hab):
     return habitacion
 
 def sql_edit_habitacion(num_hab,capacidad,acomodacion,bano):
-    strsql="UPDATE habitaciones SET hab_capacidad = '"+capacidad+"' , SET hab_acomodacion = '"+acomodacion+"' , SET hab_bano = '"+bano+"' WHERE num_habitacion = '"+num_hab+"';"
+    strsql=("UPDATE habitaciones SET hab_capacidad = ? , SET hab_acomodacion = ? , SET hab_bano = ? WHERE num_habitacion = ?",(capacidad,acomodacion,bano,num_hab))
     print(strsql)
     con=sql_connection()
     cursor_Obj=con.cursor()
@@ -37,7 +40,77 @@ def sql_edit_habitacion(num_hab,capacidad,acomodacion,bano):
     con.close()
 
 def sql_delete_habitacion(num_hab):
-    strsql="DELETE FROM habitaciones WHERE num_habitacion = '"+num_hab+"';"
+    strsql=("DELETE FROM habitaciones WHERE num_habitacion = ?",(num_hab))
+    print(strsql)
+    con=sql_connection()
+    cursor_Obj=con.cursor()
+    cursor_Obj.execute(strsql)
+    con.commit()
+    con.close()
+
+
+#reservas
+
+def sql_insert_reserva(num_doc,fecha_inicio,fecha_fin,num_hab):
+    strsql=("INSERT INTO reservaciones (num_doc,fecha_inicio,fecha_fin,num_habitacion) VALUES(?,?,?,?)",(num_doc,fecha_inicio,fecha_fin,num_hab))
+    print(strsql)
+    con=sql_connection()
+    cursor_Obj=con.cursor()
+    cursor_Obj.execute(strsql)
+    con.commit()
+    con.close()
+
+def sql_select_reserva(codigo_reserva):
+    strsql=("SELECT * FROM reservaciones WHERE codigo_reserva = ?",(codigo_reserva))
+    print(strsql)
+    con=sql_connection()
+    cursor_Obj=con.cursor()
+    cursor_Obj.execute(strsql)
+    reserva=cursor_Obj.fetchall()
+    con.close()
+    return reserva
+
+def sql_edit_reserva(num_doc,fecha_inicio,fecha_fin,num_hab,codigo_reserva):
+    strsql=("UPDATE reservaciones SET num_doc = ? , SET fecha_inicio = ? , SET fecha_fin = ?, SET num_habitacion WHERE codigo_reserva = ?",(num_doc,fecha_inicio,fecha_fin,num_hab,codigo_reserva))
+    print(strsql)
+    con=sql_connection()
+    cursor_Obj=con.cursor()
+    cursor_Obj.execute(strsql)
+    con.commit()
+    con.close()
+
+def sql_delete_reserva(codigo_reserva):
+    strsql=("DELETE FROM reservaciones WHERE codigo_reserva = ?",(codigo_reserva))
+    print(strsql)
+    con=sql_connection()
+    cursor_Obj=con.cursor()
+    cursor_Obj.execute(strsql)
+    con.commit()
+    con.close()
+
+##contacto
+
+def sql_insert_contacto(num_doc,nombre,email,comentario):
+    strsql=("INSERT INTO contacto (num_doc,nombre,email,comentario) VALUES(?,?,?,?)",(num_doc,nombre,email,comentario))
+    print(strsql)
+    con=sql_connection()
+    cursor_Obj=con.cursor()
+    cursor_Obj.execute(strsql)
+    con.commit()
+    con.close()
+
+def sql_select_contacto(num_doc):
+    strsql=("SELECT * FROM contacto WHERE num_doc = ?",(num_doc))
+    print(strsql)
+    con=sql_connection()
+    cursor_Obj=con.cursor()
+    cursor_Obj.execute(strsql)
+    contacto=cursor_Obj.fetchall()
+    con.close()
+    return contacto
+
+def sql_delete_contacto(num_doc):
+    strsql=("DELETE FROM contacto WHERE num_doc = ?",(num_doc))
     print(strsql)
     con=sql_connection()
     cursor_Obj=con.cursor()
