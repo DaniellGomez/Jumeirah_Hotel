0ea7fef0ca6d302289d9c3f1a6d906de0668be50
Camilo Urrego <ceurrego@bancofalabella.com.co>
2022-08-10 23:33:42 -0500

    Actualizacion tabla contacto

--- create_table_contacto.sql
+++ create_table_contacto.sql
@@ -1,7 +1,6 @@
 CREATE TABLE contacto (
     id_contacto INTEGER       PRIMARY KEY AUTOINCREMENT,
-    num_doc     VARCHAR       REFERENCES pass_role (num_doc) 
-                              NOT NULL,
+    num_doc     VARCHAR       NOT NULL,
     nombre      VARCHAR       NOT NULL,
     email       VARCHAR       NOT NULL,
     comentario  VARCHAR (500) 
